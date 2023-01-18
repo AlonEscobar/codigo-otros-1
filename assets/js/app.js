6ecdd33fea5a51704a65d67ac1796a63f4ae9420
Alon <alonzso07@gmail.com>
2023-01-18 11:27:59 -0600

    se realizo el ejercicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,26 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+let baseEndpoint = 'https://api.github.com';
+let usersEndpoint = `${baseEndpoint}/users`;
+let $n = document.querySelector('#name');
+let $b = document.querySelector('#blog');
+let $l = document.querySelector('#location');
 
-function displayUser(username) {
+async function displayUser(username) {
   $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  try {
+    let response = await fetch(`${usersEndpoint}/${username}`);
+    let data = await response.json();
+    $n.textContent = data.name;
+    $b.textContent = data.blog;
+    $l.textContent = data.location;
+  } catch(err) {
+    handleError(err);
+  }
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
